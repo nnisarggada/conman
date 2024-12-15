b5e3908ec43349ce9cf524a991eb62024dc32928
Nnisarg Gada <contact@nnisarg.in>
2024-12-15 15:13:43 +0530

    fix

--- lib/contact.dart
+++ lib/contact.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_contacts/flutter_contacts.dart';
 
-class ContactPage extends StatelessWidget {
+class ContactPage extends StatefulWidget {
   final Contact contact;
   const ContactPage(this.contact, {super.key});
 
   @override
+  State<ContactPage> createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  @override
   Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(title: Text(contact.displayName)),
+      appBar: AppBar(title: Text(widget.contact.displayName)),
       body: Column(children: [
-        Text('First name: ${contact.name.first}'),
-        Text('Last name: ${contact.name.last}'),
+        Text('First name: ${widget.contact.name.first}'),
+        Text('Last name: ${widget.contact.name.last}'),
         Text(
-            'Phone number: ${contact.phones.isNotEmpty ? contact.phones.first.number : '(none)'}'),
+            'Phone number: ${widget.contact.phones.isNotEmpty ? widget.contact.phones.first.number : '(none)'}'),
         Text(
-            'Email address: ${contact.emails.isNotEmpty ? contact.emails.first.address : '(none)'}'),
+            'Email address: ${widget.contact.emails.isNotEmpty ? widget.contact.emails.first.address : '(none)'}'),
       ]));
 }
